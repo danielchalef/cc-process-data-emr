f5fb9152219041b6d8b6c2605a9f6e8541d6a94f
Daniel Chalef <daniel.chalef@gmail.com>
2016-04-06 20:29:14 -0700

    standalone v1

--- cc-process-standalone.py
+++ cc-process-standalone.py
@@ -18,16 +18,24 @@ import tldextract
 # import ftfy
 import sys
 import socket
+import ujson
+import hashlib
+import uuid
+
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
 
 # import unicodedata
 # import codecs
 
 import logging
 
-logging.basicConfig(
-    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+# Retrieve from local file or S3?
+REMOTE = False
+OUTPUT_PATH = './out'
 
-# mrjob.util.log_to_stream(stream=sys.stderr, level=logging.DEBUG, debug=True)
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
 domains = ['fortune', 'readwrite', 'recode',
            'techcrunch', 'thenextweb', 'venturebeat']
@@ -35,81 +43,93 @@ domains = ['fortune', 'readwrite', 'recode',
 
 def process_record(record):
     # logging.debug("Processing record")
-    try:
-        if record['content-type'] != 'text/plain':
-            logging.debug("Skipping: " + record['content-type'])
-            return
-        url = record.header.get('warc-target-uri', None)
-        # logging.debug("Processing record with URL " + url)
-        if url:
-            domain = tldextract.extract(url).domain
-            if domain in domains:
-                logging.debug("Matches domain list: " + domain)
-                payload = record.payload.read().decode('utf-8', errors='strict')
-                # payload = record.payload.read()
-                # logging.info("Text encoding: " + str(type(payload)))
-                # payload = deaccent(payload)
-                # yield {"domain": domain}, transform_doc(nlp(payload))
-                logging.debug(
-                    "Successfully extracted paylor for domain: " + domain)
-                yield {"domain": domain}, payload
-    except IOError:
-        logging.error("WARC IO Error: " + str(IOError))
-        yield {'domain': 'error'}, ''
-    except Exception as e:
-        logging.error("WARC IO Error: " +
-                      traceback.format_exception(*sys.exc_info()))
-        yield {'domain': 'error'}, ''
+    if record['content-type'] != 'text/plain':
+        logging.debug("Skipping: " + record['content-type'])
+        return(False)
+    url = record.header.get('warc-target-uri', None)
+    # logging.debug("Processing record with URL " + url)
+    if url:
+        domain = tldextract.extract(url).domain
+        if domain in domains:
+            logging.debug("Matches domain list: " + domain)
+            payload = record.payload.read().decode('utf-8', errors='strict')
+            # payload = record.payload.read()
+            # logging.info("Text encoding: " + str(type(payload)))
+            # payload = deaccent(payload)
+            # yield {"domain": domain}, transform_doc(nlp(payload))
+            logging.debug(
+                "Successfully extracted paylor for domain: " + domain)
+            return(dict([('domain', domain), ('payload', payload)]))
+    else:
+        return(False)
 
-def mapper(line):
-    try:
-        # Connect to Amazon S3 using anonymous credentials
-        conn = boto.connect_s3(anon=True, host='s3.amazonaws.com')
-        logging.debug("Successfully opened connection to s3")
 
-        pds = conn.get_bucket('aws-publicdatasets', validate=False)
-        logging.debug("Bucket is aws-puiblicdatasets")
 
-        # Start a connection to one of the WARC files
-        # k = boto3.resource('s3').Object('aws-publicdatasets', line).get()['Body']
-        k = Key(pds, line)
-        logging.debug("Got key for line: " + line)
-        f = warc.WARCFile(fileobj=GzipStreamFile(k))
-        logging.debug("Got a warc file stream for: " + line)
-    except Exception:
-        logging.error("WARC Open from S3 Error for line {}:\n{}"
-                      .format(line,  traceback.format_exception(*sys.exc_info())))
-        yield {'domain': 'error'}, ['']
+def process_file(line):
+    output_contents = []
+    line = line.strip()
+    if REMOTE:
+        try:
+            # Connect to Amazon S3 using anonymous credentials
+            conn = boto.connect_s3(anon=True, host='s3.amazonaws.com')
+            logging.debug("Successfully opened connection to s3")
 
+            pds = conn.get_bucket('aws-publicdatasets', validate=False)
+            logging.debug("Bucket is aws-puiblicdatasets")
+
+            # Start a connection to one of the WARC files
+            k = Key(pds, line)
+            logging.debug("Got key for line: " + line)
+            f = warc.WARCFile(fileobj=GzipStreamFile(k))
+            logging.debug("Got a warc file stream for: " + line)
+        except Exception:
+            logging.error("WARC Open from S3 Error for line {}:\n{}"
+                          .format(line,  traceback.format_exception(*sys.exc_info())))
+            return(dict([('line', line), ('success', False)]))
         # If we're local, use files on the local file system
         # requires absolute path to files
-        # else:
-        #     try:
-        #         logging.info('Loading local file {}'.format(line))
-        #         f = warc.WARCFile(fileobj=gzip.open(line, 'rb'))
-        #         logging.debug("Got a warc file stream for: " + line)
-        #     except Exception as e:
-        #         logging.error("WARC Open from S3 Error for line {}:\n{}"
-        #                       .format(line,  traceback.format_exception(*sys.exc_info())))
-        #         yield {'domain': 'error'}, ['']
+    else:
+            try:
+                logging.info('Loading local file {}'.format(line))
+                f = warc.WARCFile(fileobj=gzip.open(line, 'rb'))
+                logging.debug("Got a warc file stream for: " + line)
+            except Exception as e:
+                logging.error("WARC Open from S3 Error for line {}:\n{}"
+                              .format(line,  traceback.format_exception(*sys.exc_info())))
+                return(dict([('line', line), ('success', False)]))
 
+    logging.debug("Iterating on f for line: " + line)
+    try:
+        for record in f:
+            # logging.debug("Processing record for line: " + line)
+            value = process_record(record)
+            if value:
+                logging.debug("Got content from for domain: {}\nline: {}"
+                              .format(value['domain'], line))
+                output_contents.append(value)
+        if len(output_contents) > 0:
+            out_filename = "{}/{}".format(OUTPUT_PATH,
+                                          hashlib.md5(line).hexdigest())
+            with open(out_filename, 'wb') as out_file:
+                ujson.dump(output_contents, out_file)
+    except Exception:
+        logging.error("WARC iterate error for line {}:\n{}"
+                      .format(line,  traceback.format_exception(*sys.exc_info())))
+        return(dict([('line', line), ('success', False)]))
 
-        logging.debug("Iterating on f for line: " + line)
-        try:
-            for record in f:
-                # logging.debug("Processing record for line: " + line)
-                for key, value in self.process_record(record):
-                    logging.debug(u"Got a key value pair for line: {}\nkey: {}"
-                                  .format(line, key))
-                    yield key, value
-        except Exception:
-            logging.error("WARC iterate error for line {}:\n{}"
-                          .format(line,  traceback.format_exception(*sys.exc_info())))
-            yield {'domain': 'error'}, ['']
+    return(dict([('line', line), ('success', True)]))
 
-    def reducer(self, domain, values):
-        logging.debug("Running reducer for key: " + str(domain))
-        yield domain, {"text": list(values)}
 
 if __name__ == '__main__':
-    MRArticleExtractor.run()
+    lines = sys.stdin.readlines()
+
+    pool = Pool(10)
+
+    results = pool.map(process_file, lines)
+    log_filename = "{}/{}.log".format(OUTPUT_PATH,
+                                  hashlib.md5(''.join(lines)).hexdigest())
+    with open(log_filename, 'wb') as out_file:
+        ujson.dump(results, out_file)
+
+    pool.close()
+    pool.join()
